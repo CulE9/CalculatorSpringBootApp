f35f82121568a0a2683cc03e33683e8649e2fdf7
CulE <undertaker007@onet.pl>
2019-02-14 15:38:24 +0100

    controller updated, add, sub

--- src/main/java/com/cule/calculator/controller/CalculatorController.java
+++ src/main/java/com/cule/calculator/controller/CalculatorController.java
@@ -1,4 +1,36 @@
 package com.cule.calculator.controller;
 
+import com.cule.calculator.models.CalculatorOperations;
+import com.cule.calculator.models.OperationModel;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class CalculatorController {
+
+    CalculatorOperations calculatorOperations = new CalculatorOperations();
+    OperationModel operationModel = new OperationModel();
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("operationModel", operationModel);
+        return "index";
+    }
+
+    @RequestMapping(params = "add", method = RequestMethod.POST)
+    public String add(@ModelAttribute("operationModel") OperationModel operationModel, Model model) {
+        model.addAttribute("result", calculatorOperations.add(operationModel.getA(), operationModel.getB()));
+        return "index";
+    }
+
+    @RequestMapping(params = "sub", method = RequestMethod.POST)
+    public String substract(@ModelAttribute("operationModel") OperationModel operationModel, Model model) {
+        model.addAttribute("result", calculatorOperations.subtract(operationModel.getA(), operationModel.getB()));
+        return "index";
+    }
+
 }
